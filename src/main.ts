b5e2d05ec1f5543859e6e837fb67047a940b34ab
Jhon Ospina <jhonospina2711@gmail.com>
2024-08-10 23:03:31 -0500

    Se cargan corrigen problemas de compatibilidad

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-<<<<<<< HEAD
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-=======
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
-})
->>>>>>> 5dbefe8 (initial commit)
   .catch(err => console.error(err));
