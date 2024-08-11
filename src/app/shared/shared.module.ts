7c972bc2a2111aa22d626b8bb3a36705f9a7c8b7
Jhon Ospina <jhonospina2711@gmail.com>
2024-08-11 01:17:39 -0500

    Se adiciona codigo Sección 9 - temas: Modulo y Paginas SPA, TouterLink y RouterLinkOptions

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RouterModule } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+
+@NgModule({
+  declarations: [
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
