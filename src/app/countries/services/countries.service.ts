6a152e06bc23d72bda0ee78a0b7b08cc77c54ec6
Jhon Ospina <jhonospina2711@gmail.com>
2024-08-17 23:45:41 -0500

    Se crea la barra de busqueda, la cual se conecta con el api restcountries y muestra una tabla con los nombres de las capitales

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private apiURL: string = 'https://restcountries.com/v3.1'
+  http: any;
+
+  constructor(private httpClient: HttpClient) { }
+
+  searchCapital( term: string ): Observable<Country[]>{
+
+    const url = `${ this.apiURL}/capital/${ term }`;
+    return this.httpClient.get<Country[]>( url);
+
+  }
+
+}
