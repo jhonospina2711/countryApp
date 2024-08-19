2ca0eaeac1a9d858e4ca5441d3717f7008896172
Jhon Ospina <jhonospina2711@gmail.com>
2024-08-19 01:11:12 -0500

    Seccion 9 - Final de la sección 9, se adiciona la función de buscar por la información del país

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -11,10 +11,46 @@ export class CountriesService {
 
   constructor(private httpClient: HttpClient) { }
 
+  searchCountryByAlphaCode( code: string): Observable<Country | null> {
+
+    const url = `${ this.apiURL}/alpha/${ code }`;
+
+    return this.httpClient.get<Country[]>( url)
+    .pipe(
+      map( countries => countries.length > 0 ? countries[0] : null),
+      catchError(error => of(null))
+    );
+
+  }
+
   searchCapital( term: string ): Observable<Country[]>{
 
     const url = `${ this.apiURL}/capital/${ term }`;
-    return this.httpClient.get<Country[]>( url);
+
+    return this.httpClient.get<Country[]>( url)
+      .pipe(
+        catchError(error => of([]))
+      );
+  }
+
+  searchCountry(term: string): Observable<Country[]> {
+
+    const url = `${ this.apiURL}/name/${ term }`;
+
+    return this.httpClient.get<Country[]>( url)
+    .pipe(
+      catchError(error => of([]))
+    );
+  }
+
+  searchRegion(term: string): Observable<Country[]> {
+
+    const url = `${ this.apiURL}/region/${ term }`;
+
+    return this.httpClient.get<Country[]>( url)
+    .pipe(
+      catchError(error => of([]))
+    );
 
   }
 
